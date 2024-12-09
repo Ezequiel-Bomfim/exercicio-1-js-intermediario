334011f6c5b27512a6d7966775a16c72a09c66c2
Ezequiel Bomfim <ezequiel88er@gmail.com>
2024-12-09 16:31:37 -0300

    Inserindo arquivos novos

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+let botaoDeTrocarCor = document.getElementById("trocar-de-cor")
+botaoDeTrocarCor.addEventListener('click', function trocarCor() {
+    let backgroundQuadradoAlterado = document.querySelector('.div-azul')
+    backgroundQuadradoAlterado.classList.add('background-alterado')
+})
+
+
+
